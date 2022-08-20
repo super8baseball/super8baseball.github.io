e25b2c443879c435e3a15c4790f233cadd151c02
KJ-Chiu <kj.chiu73@gmail.com>
2022-08-20 13:36:32 +0800

    feat: add season groups

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import CoverJPG from './assets/cover.jpg';
 import Game from './features/Game';
 import Overview from './features/Overview';
 
+const TOTAL_KEY = 'OPTION_TOTAL';
+
 const fetchGames = (fileNames) => {
   const fetchGame = (name) => {
     return new Promise((resolve, reject) => {
@@ -30,7 +32,9 @@ const App = () => {
   const [sumBatters, setSumBatters] = useState([]);
   const [sumPitchers, setSumPitchers] = useState([]);
   const [games, setGames] = useState([]);
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [selectedIndex, setSelectedIndex] = useState(TOTAL_KEY);
+
+  const [seasonGroups, setSeasonGroups] = useState([]);
 
   useEffect(() => {
     axios.get('/resources/list.txt').then(({ data }) => {
@@ -43,6 +47,8 @@ const App = () => {
         setGames(sortedGames);
         setSumBatters(sumupBatters(sortedGames.map((g) => g.batters)));
         setSumPitchers(sumupPitchers(sortedGames.map((g) => g.pitchers)));
+
+        setSeasonGroups(R.groupBy((game) => game.info.season, sortedGames));
       });
     });
   }, []);
@@ -60,7 +66,10 @@ const App = () => {
       <div className="content">
         <div className="tab-switch">
           <select value={selectedIndex} onChange={(e) => setSelectedIndex(e.target.value)}>
-            <option value={-1}>成績總覽</option>
+            <option value={TOTAL_KEY}>成績總覽</option>
+            {Object.keys(seasonGroups).map((seasonName) => (
+              <option key={seasonName} value={seasonName}>{`賽事總覽 - ${seasonName}`}</option>
+            ))}
             {games.map((game, gameIndex) => (
               <option key={gameIndex} value={gameIndex}>{`${game.info.season} ${game.info.date.format('YYYY/MM/DD')} ${
                 game.info.away
@@ -68,10 +77,17 @@ const App = () => {
             ))}
           </select>
         </div>
-        {games[selectedIndex] ? (
-          <Game game={games[selectedIndex]} />
+        {selectedIndex === TOTAL_KEY ? (
+          <Overview key={selectedIndex} games={games} sumBatters={sumBatters} sumPitchers={sumPitchers} />
+        ) : R.is(String, selectedIndex) && seasonGroups[selectedIndex] ? (
+          <Overview
+            key={selectedIndex}
+            games={seasonGroups[selectedIndex]}
+            sumBatters={sumupBatters(seasonGroups[selectedIndex].map((g) => g.batters))}
+            sumPitchers={sumupPitchers(seasonGroups[selectedIndex].map((g) => g.pitchers))}
+          />
         ) : (
-          <Overview games={games} sumBatters={sumBatters} sumPitchers={sumPitchers} />
+          <Game key={selectedIndex} game={games[selectedIndex]} />
         )}
       </div>
     </StyledApp>
